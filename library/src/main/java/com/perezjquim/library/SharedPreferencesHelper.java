b04369852e5970aaf3f484bb1b78d30b3a7d08e9
Joaquim Perez <jquim1@hotmail.com>
2018-03-13 23:04:07 +0000

    The beginning of them all

---
+++ library/src/main/java/com/perezjquim/library/SharedPreferencesHelper.java
@@ -0,0 +1,30 @@
+package com.perezjquim.library;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SharedPreferencesHelper
+{
+    private Context context;
+    public SharedPreferencesHelper(Context context)
+    {
+        this.context = context;
+    }
+
+    private SharedPreferences getSharedPreferences(String xml)
+    {
+        return context.getSharedPreferences(xml, Context.MODE_PRIVATE);
+    }
+
+    public String getString(String xml, String key)
+    {
+        return getSharedPreferences(xml).getString(key,null);
+    }
+
+    public void setString(String xml, String key, String value)
+    {
+        SharedPreferences.Editor prefs = getSharedPreferences(xml).edit();
+        prefs.putString(key,value);
+        prefs.apply();
+    }
+}
